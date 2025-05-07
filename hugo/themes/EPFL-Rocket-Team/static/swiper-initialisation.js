8a9ffec3da9223a59cc598cd9b8f68294a4ab133
Jordan Warne <JordanWarne04>
2025-05-07 08:38:38 +0200

    no more inline js and css in header

---
+++ hugo/themes/EPFL-Rocket-Team/static/swiper-initialisation.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    pagination: { el: ".swiper-pagination" },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    scrollbar: { el: ".swiper-scrollbar" },
+  });
+});
